60c77e4262f845d62051b107f29ea55080168fce
alesanm <alesan.med@yandex.ru>
2023-07-27 16:29:25 +0200

    rm additional functionality

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from PIL import Image
 import pandas as pd
 from model import AutomateLablingModel
 
-def load_model():
-    with st.spinner('Model is loading...'):
-        model = AutomateLablingModel()
-    st.success('Done!')
-    return model
-
 def get_pil_image(file):
     file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
@@ -19,48 +13,48 @@ def get_pil_image(file):
     return pil_image
 
 def get_bar_percent(current_index, images_number):
-    return int((current_index+1)*100/images_number)
-
+    return int(current_index*100/images_number)
 
 st.title("Let's describe your fashion photos!")
-csv = pd.DataFrame(data=[])
+
+# load the model
+with st.spinner('Model is loading...'):
+    model = AutomateLablingModel()
+st.success('Done!')
 
 
 pil_images = []
-with st.form('input form'):
-    model = load_model()
-    uploaded_files = st.file_uploader("Choose your images", type=["jpg", "png"], accept_multiple_files=True)
+image_descriptions = []
+output_csv = pd.DataFrame({'image':pil_images, 'description':image_descriptions})
+submit_button = None
 
+with st.form('input form'):
+    uploaded_files = st.file_uploader("Choose your images", type=["jpg", "png"], accept_multiple_files=True)
     if uploaded_files is not None:
         for file in uploaded_files:
             pil_image = get_pil_image(file)
             pil_images.append(pil_image)
 
     submit_button = st.form_submit_button('Submit')
-    if submit_button:
-        progress_text = "Building descriptions..."
-        my_bar = st.progress(0, text=progress_text)
-        to_save = []
-        image_descriptions = []
-        with st.container():
-            for idx, pil_image in enumerate(pil_images):
 
-                my_bar.progress(get_bar_percent(idx, len(pil_images)), text=progress_text)
+if submit_button:
+    progress_text = "Building descriptions..."
+    my_bar = st.progress(0, text=progress_text)
 
-                description = model.get_label(pil_image)
+    for idx, pil_image in enumerate(pil_images):
+        # redraw progress bar
+        my_bar.progress(get_bar_percent(idx, len(pil_images)), text=progress_text)
+        # build description
+        description = model.get_label(pil_image)
+        # draw image with the description
+        st.image(pil_image, caption=description, width=400)
+        image_descriptions.append(description)
 
-                st.image(pil_image, caption=description, width=300)
-                to_save.append(st.checkbox('save', value=True, key=idx))
-                image_descriptions.append(description)
-
-        csv = pd.DataFrame({'image':pil_image, 'description':image_descriptions})
-        csv['Save'] = to_save
-        output_csv = csv[csv['Save'] == True]
-
-st.download_button(
-    label="Download descriptions in CSV",
-    data=csv,
-    file_name='descriptions.csv',
-    mime='text/csv',
-)
-
+    output = pd.DataFrame({'description': image_descriptions})
+    csv = output.to_csv().encode('utf-8')
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='descriptions.csv',
+        mime='text/csv',
+    )
